fd8b0df9d8167c80bd58ba1ed80626c12500432b
wujinsheng1 <wujinsheng1@yy.com>
2020-07-08 11:32:13 +0800

    更换UI

---
+++ app/src/main/java/com/salton123/recorder/page/SettingPage.kt
@@ -0,0 +1,12 @@
+package com.salton123.recorder.page
+
+import com.salton123.ui.base.BaseFragment
+import net.yrom.screenrecorder.R
+
+class SettingPage : BaseFragment() {
+    override fun getLayout(): Int = R.layout.page_setting
+
+    override fun initViewAndData() {
+        
+    }
+}
